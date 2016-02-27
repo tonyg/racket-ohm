c01e9123f09a5f81a0b7e183618ad027f499fe4b
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-02-27 18:33:42 -0500

    Syntactic and lexical rule names.

--- ohm/grammar.rkt
+++ ohm/grammar.rkt
@@ -2,7 +2,9 @@
 ;; Ohm grammar structure and basic functions.
 
 (provide (struct-out ohm-grammar)
-         (struct-out ohm-rule))
+         (struct-out ohm-rule)
+         rule-name-syntactic?
+         rule-name-lexical?)
 
 (struct ohm-grammar
   (name
@@ -18,3 +20,24 @@
    formals
    body)
   #:transparent)
+
+;; Rules are either "syntactic", meaning they deliberately discard
+;; whitespace within embedded sequences, or "lexical", meaning
+;; whitespace must be treated explicitly by the rule.
+;;
+;; Ohm's rule names start with capital letters exactly when they are
+;; "syntactic".
+(define (rule-name-syntactic? n)
+  (define s (symbol->string n))
+  (and (positive? (string-length s))
+       (char-upper-case? (string-ref s 0))))
+
+(define (rule-name-lexical? n)
+  (not (rule-name-syntactic? n)))
+
+(module+ test
+  (require rackunit)
+  (check-true (rule-name-syntactic? 'SyntacticRule))
+  (check-false (rule-name-syntactic? 'lexicalRule))
+  (check-false (rule-name-lexical? 'SyntacticRule))
+  (check-true (rule-name-lexical? 'lexicalRule)))
